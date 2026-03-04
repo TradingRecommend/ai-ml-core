a8dfd445f93a8cddf7f50fc6e0f37261a5cb28a4
nelsonvu <vunam722000@gmail.com>
2026-03-04 23:26:49 +0700

    update requirement

--- api/cron.py
+++ api/cron.py
@@ -8,6 +8,7 @@ from src.services.etl.stock.etl_prediction_feature import ETLPredictionStockFeat
 from src.services.noti.stock.telegram import StockTelegramNotifier
 from src.services.prediction.composite import PredictionComposite
 from src.services.prediction.stock.stock_logistic_prediction import StockLogisticPrediction
+os.environ["HOME"] = "/tmp"
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
